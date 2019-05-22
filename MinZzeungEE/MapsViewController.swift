06d7786076782e8be493140aa5408130bc37f829
cadenzah <cadenzah93@gmail.com>
2019-05-22 21:24:06 +0900

    2번째 탭의 Google Map 관련 기능 추가 - 처음 탭 열었을 때 현재 내 위치에서 지도 열리기 - 나침반 버튼, 현재 내 위치로 이동하기 버튼 추가

--- MinZzeungEE/MapsViewController.swift
+++ MinZzeungEE/MapsViewController.swift
@@ -19,28 +19,52 @@ class MapsViewController: UIViewController {
     }
     
     override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: 37.5696, longitude: 126.98521819999996, zoom: 17.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
         
-        // 사용자 위치정보 허락 모달
+        // Ask permission to use user's location info
+        // Popup the modal
         self.locationManager.requestAlwaysAuthorization()
-        mapView.isMyLocationEnabled = true
         
-        // Creates a marker in the center of the map.
-        let restraunt1 = GMSMarker()
-        restraunt1.position = CLLocationCoordinate2D(latitude: 37.5696, longitude: 126.98521819999996)
-        restraunt1.title = "경성주막"
-        restraunt1.snippet = "왕십리"
-        restraunt1.map = mapView
+        // Enable tracking the user's location in real-time
+        self.locationManager.startUpdatingLocation()
         
-        let restraunt2 = GMSMarker()
-        restraunt2.position = CLLocationCoordinate2D(latitude: 37.5690, longitude: 126.98521819999996)
-        restraunt2.title = "백번"
-        restraunt2.snippet = "왕십리"
-        restraunt2.map = mapView
-        //마커추가하는법
-        //내위치로 이동
+        if let latitude = self.locationManager.location?.coordinate.latitude,
+            let longitude = self.locationManager.location?.coordinate.longitude {
+            
+            // reference for user's current real-time location
+            let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 16.0)
+            let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+            self.view = mapView
+            
+            // Enable map to use user's location
+            mapView.isMyLocationEnabled = true
+            
+            // Create a button which shows user's current location by marker in the map
+            mapView.settings.myLocationButton = true
+            // Show compass in the map
+            mapView.settings.compassButton = true
+            
+            
+            // Creates a marker in the center of the map.
+            let restraunt1 = GMSMarker()
+            restraunt1.position = CLLocationCoordinate2D(latitude: 37.5696, longitude: 126.98521819999996)
+            restraunt1.title = "경성주막"
+            restraunt1.snippet = "왕십리"
+            restraunt1.map = mapView
+            
+            let restraunt2 = GMSMarker()
+            restraunt2.position = CLLocationCoordinate2D(latitude: 37.5690, longitude: 126.98521819999996)
+            restraunt2.title = "백번"
+            restraunt2.snippet = "왕십리"
+            restraunt2.map = mapView
+            
+            //마커추가하는법
+            // 지도 상에서 마커를 추가할 수 있도록
+            
+            //내위치로 이동
+            
+        }
+        
+        
     }
     
 }
