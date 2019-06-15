50f7c3ef8c7d6faf9f0b993aa2eda907fc009216
cadenzah <cadenzah93@gmail.com>
2019-06-15 22:56:41 +0900

    new view controller for 2nd tab

---
+++ MinZzeungEE/StoreMapController.swift
@@ -0,0 +1,30 @@
+//
+//  StoreMapController.swift
+//  MinZzeungEE
+//
+//  Created by cadenzah on 15/06/2019.
+//  Copyright © 2019 SungIn. All rights reserved.
+//
+
+import UIKit
+
+class StoreMapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
